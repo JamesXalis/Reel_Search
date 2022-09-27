6a141eca4ff31ea4fbb54c27209dbdea93cb9169
William Wareing <wwareing11@gmail.com>
2022-09-27 14:32:27 -0700

    Added a black/red split background, added 2 carousels of Macho Man Randy Savage, offset both diagonally

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,7 @@
 console.log ('this is loaded')
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
